744e8c169380f58fd0a7af4013ca09b2d94b7095
Lim Min Jie <adronlim@hotmail.com>
2019-04-24 10:27:31 +0800

    -Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChartComponent } from './chart/chart.component';
+import { DisplayComponent } from './display/display.component';
+import { PanelLayoutComponent } from './panel-layout/panel-layout.component';
+import { ChartComponentComponent } from './chart-component/chart-component.component';
+import { ChartDirectiveDirective } from './chart-directive.directive';
+import { HistogramChartComponent } from './histogram-chart/histogram-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    DisplayComponent,
+    PanelLayoutComponent,
+    ChartComponentComponent,
+    ChartDirectiveDirective,
+    HistogramChartComponent
   ],
   imports: [
     BrowserModule
